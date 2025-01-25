9a3dd6a8c6dde953b17a0f9c05f7b1db1ad5ba17
SwaibaFaisal <swaiba2.faisal@live.uwe.ac.uk>
2025-01-25 13:48:53 +0000

    Updated Movement

--- Game Jam Bubble/Assets/Player/Scripts/Player Movement.cs
+++ Game Jam Bubble/Assets/Player/Scripts/Player Movement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro.EditorUtilities;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -14,63 +15,108 @@ public class PlayerMovement : MonoBehaviour
 
     //private values
     Vector2 m_walkValue;
-    E_PlayerWalkStates m_walkStates;
-    E_PlayerWalkStates m_previousWalkStates;
-    Rigidbody2D m_rigidBody;
+    Vector2 m_walkDirection;
+    Vector2 m_previousDirection;
+    SpriteRenderer m_spriteRenderer;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        m_rigidBody = this.GetComponent<Rigidbody2D>();
-        
+        m_spriteRenderer = this.GetComponent<SpriteRenderer>();  
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 a = m_walkValue * m_speed;
+        Vector3 a = m_walkDirection * m_speed;
         this.transform.position += a;
-
 
        
     }
 
 
-
-    public void OnWalkHorizontal(InputAction.CallbackContext _context)
-    {
-        if(_context.started)
-        {
-            m_walkStates = E_PlayerWalkStates.HORIZONTAL;
-            m_walkValue.x = _context.ReadValue<Vector2>().x;
-        }
-
-        if(_context.canceled)
-        {
-            m_walkStates = m_previousWalkStates;
-            m_previousWalkStates = E_PlayerWalkStates.HORIZONTAL;
-        }
-    }
-
-    public void OnWalkVertical(InputAction.CallbackContext _context)
-    {
-        if(_context.started)
-        {
-            m_walkStates = E_PlayerWalkStates.VERTICAL;
-            m_walkValue.y = _context.ReadValue<Vector2>().y;
-        }
-
-        if(_context.canceled)
-        {
-            m_walkStates = m_previousWalkStates;
-            m_previousWalkStates = E_PlayerWalkStates.VERTICAL;
-        }
-    }
-
     public void OnMovement(InputAction.CallbackContext _context)
     {
-        m_walkValue = _context.ReadValue<Vector2>();
+        if(_context.ReadValue<Vector2>() == Vector2.zero)
+        {
+            m_walkDirection = Vector2.zero;
+        }
+        m_previousDirection = _context.ReadValue<Vector2>();
+    }
+
+    public void OnLeftPressed(InputAction.CallbackContext _context)
+    {
+        if(_context.started)
+        {
+            m_walkDirection = Vector2.left;
+            m_spriteRenderer.flipX = true;
+        }
+        if (_context.canceled)
+        {
+            m_walkDirection.x = m_previousDirection.x;
+        }
+    }
+
+    public void OnRightPressed(InputAction.CallbackContext _context)
+    {
+        if (_context.started)
+        {
+            m_walkDirection = Vector2.right;
+            m_spriteRenderer.flipX = false;
+        }
+        if (_context.canceled)
+        {
+            m_walkDirection.x = m_previousDirection.x;
+        }
+
+    }
+    public void OnUpPressed(InputAction.CallbackContext _context)
+    {
+        if (_context.started)
+        {
+            m_walkDirection = Vector2.up;
+        }
+        if(_context.canceled)
+        {
+            m_walkDirection.y = m_previousDirection.y;
+        }
+    }
+    public void OnDownPressed(InputAction.CallbackContext _context)
+    {
+        if (_context.started)
+        {
+            m_walkDirection = Vector2.down;
+        }
+        if (_context.canceled)
+        {
+            m_walkDirection.y = m_previousDirection.y;
+        }
+    }
+
+    Vector2 GetWalkDirection(Vector2 _input)
+    {
+        Vector2 _finalDirection = Vector2.zero;
+
+        if(_input.y > 0.01f)
+        {
+            _finalDirection = Vector2.up;
+        }
+        else if (_input.y < -0.01f)
+        {
+            _finalDirection = Vector2.down;
+        }
+        else if (_input.x > 0.01f)
+        {
+            _finalDirection = Vector2.right;
+        }
+        else if (_input.x < -0.01f)
+        {
+            _finalDirection = Vector2.left;
+        }
+       
+        return _finalDirection;
     }
 
 }
