6ef27506627dc9b95bd04458815d237a13a87a61
SwaibaFaisal <swaiba2.faisal@live.uwe.ac.uk>
2025-01-26 02:10:54 +0000

    .
    :)

--- Game Jam Bubble/Assets/Player/Scripts/Player Health.cs
+++ Game Jam Bubble/Assets/Player/Scripts/Player Health.cs
@@ -6,19 +6,38 @@ public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] int m_maxHearts;
     [SerializeField] LayerMask m_enemyLayer;
+    int m_currentHearts;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        RestartRound();
     }
 
     // Update is called once per frame
     void Update()
     {
-      
+        if (Physics2D.OverlapCircle(this.transform.position, 0.5f, m_enemyLayer) != null)
+        {
+            Hit();
+
+        }
     }
 
-   
+    public void RestartRound()
+    {
+        m_currentHearts = m_maxHearts;
+    }
+
+    public void Hit()
+    {
+        this.transform.position = Vector2.zero;
+        m_currentHearts--;
+        if(m_currentHearts <= 0)
+        {
+            print("Death");
+        }
+    }
+
 }
