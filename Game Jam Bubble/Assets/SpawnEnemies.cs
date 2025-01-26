d6695860f45e2dc837eb6f56d321c0b5193b1c78
Lucy <lucy6.johnson@live.uwe.ac.uk>
2025-01-26 16:57:33 +0000

    Fixed hearts and enemy spawning

--- Game Jam Bubble/Assets/SpawnEnemies.cs
+++ Game Jam Bubble/Assets/SpawnEnemies.cs
@@ -12,6 +12,7 @@ public class SpawnEnemies : MonoBehaviour
 
     public float m_spawnTimerMax = 8;
     public float m_spawnTimer = 8;
+
     public bool spawnTimerRunning;
 
     private GameObject m_enemyToSpawn;
@@ -21,6 +22,8 @@ public class SpawnEnemies : MonoBehaviour
     void Start()
     {
         spawnTimerRunning = true;
+        m_spawnTimer = 0;
+
     }
 
     // Update is called once per frame
@@ -38,18 +41,20 @@ public class SpawnEnemies : MonoBehaviour
                 {
                     m_spawnPosition = m_spawnPositions[Random.Range(0, m_spawnPositions.Length)];
 
-                    m_enemyToSpawn = Instantiate (m_enemyPrefab);
+                    m_enemyToSpawn = Instantiate(m_enemyPrefab);
 
-                   
+
                     Enemy _script = m_enemyToSpawn.GetComponent<Enemy>();
-                    if (_script != null )
+
+                    if (_script != null)
                     {
                         _script.m_player = player;
                     }
-                    m_enemies.Add(m_enemyToSpawn);
-                    
-                }
 
+                    m_enemyToSpawn.transform.position = m_spawnPosition.transform.position;
+                    m_enemies.Add(m_enemyToSpawn);
+
+                }
                 m_spawnTimer = m_spawnTimerMax;
             }
          
@@ -68,15 +73,18 @@ public class SpawnEnemies : MonoBehaviour
             }
             
         }
+
         m_enemies.Clear();
 
         for (int i = 0; i < m_spawnPositions.Length; i++)
         { 
            m_spawnPosition = m_spawnPositions[Random.Range(0, m_spawnPositions.Length)];
+
            m_enemyToSpawn = Instantiate(m_enemyPrefab);
 
 
             Enemy _script = m_enemyToSpawn.GetComponent<Enemy>();
+
             if (_script != null)
             {
                 _script.m_player = player;
